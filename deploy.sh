e88ba6fdf3b45660c40ed98b63c62794e6da8c09
Marcos Gabarda <hey@marcosgabarda.com>
2017-10-17 09:58:23 +0200

    deploy script for AWS

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Variables
+# ------------------------------------------------------------------------------
+readonly PACKAGE_VERSION="v$(node -p -e "require('./package.json').version")"
+readonly AWS_S3_BUCKET="sdk.10darts.com"
+
+# Buld and upload
+# ------------------------------------------------------------------------------
+npm run build
+aws --profile 10darts s3 cp dist/10dartsSDK.js s3://${AWS_S3_BUCKET}/${PACKAGE_VERSION}/
+aws --profile 10darts s3 cp dist/10dartsServiceWorker.js s3://${AWS_S3_BUCKET}/${PACKAGE_VERSION}/
