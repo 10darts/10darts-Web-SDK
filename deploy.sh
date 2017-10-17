6aeb14364ce17bb3185fc56cec534783071f275a
Marcos Gabarda <hey@marcosgabarda.com>
2017-10-17 17:46:01 +0200

    minor

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ readonly AWS_S3_BUCKET="sdk.10darts.com"
 # Buld and upload
 # ------------------------------------------------------------------------------
 npm run build
-aws --profile 10darts s3 cp dist/10dartsSDK.js s3://${AWS_S3_BUCKET}/${PACKAGE_VERSION}/
-aws --profile 10darts s3 cp dist/10dartsServiceWorker.js s3://${AWS_S3_BUCKET}/${PACKAGE_VERSION}/
+aws --profile 10darts s3 cp dist/10dartsSDK.js s3://${AWS_S3_BUCKET}/${PACKAGE_VERSION}/ --acl public-read
+aws --profile 10darts s3 cp dist/10dartsServiceWorker.js s3://${AWS_S3_BUCKET}/${PACKAGE_VERSION}/ --acl public-read
