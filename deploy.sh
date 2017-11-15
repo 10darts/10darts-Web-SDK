b5b4a4654adfdbd222f7dc110162bb0611a62240
Salvador Marí <saruba@gmail.com>
2017-11-15 13:43:53 +0100

    clear cache in deploy

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@
 # ------------------------------------------------------------------------------
 readonly PACKAGE_VERSION="v$(node -p -e "require('./package.json').release")"
 readonly AWS_S3_BUCKET="sdk.10darts.com"
-
+readonly CLOUDFRONT_DISTRIBUTION_ID="EJQDLWYC89E1C"
 # Buld and upload
 # ------------------------------------------------------------------------------
 npm run build
 aws --profile 10darts s3 cp dist/10dartsSDK.js s3://${AWS_S3_BUCKET}/${PACKAGE_VERSION}/ --acl public-read
 aws --profile 10darts s3 cp dist/10dartsServiceWorker.js s3://${AWS_S3_BUCKET}/${PACKAGE_VERSION}/ --acl public-read
+aws --profile 10darts cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths /${PACKAGE_VERSION}/*
