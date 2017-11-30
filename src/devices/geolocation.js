9846015a020e0062b72abdf0aa54b6df2d8759e8
Salvador Marí <saruba@gmail.com>
2017-11-30 13:54:10 +0100

    update version

--- src/devices/geolocation.js
+++ src/devices/geolocation.js
@@ -2,6 +2,7 @@ import { logger, store, patch } from '../utils';
 
 export default function geolocation(device = store.device) {
   if (store.geolocation && 'geolocation' in navigator) {
+    if (!device) { throw new Error('You need a device code to send geolocation'); }
     logger('Getting geolocation');
     navigator.geolocation.getCurrentPosition(({ coords }) => {
       logger('Geolocation', coords);
